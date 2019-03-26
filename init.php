d916428d4a6415e235d5d537220d03b065c061da
VladT <v.tertichniy@epages.in.ua>
2019-03-27 00:21:11 +0200

    fdf

--- init.php
+++ init.php
@@ -1,13 +1,3 @@
 <?php
 
-checkJSON($chatID,$update);
-
-function checkJSON($chatID,$update){
-
-    $myFile = "log.txt";
-    $updateArray = print_r($update,TRUE);
-    $fh = fopen($myFile, 'a') or die("can't open file");
-    fwrite($fh, $chatID ."\n\n");
-    fwrite($fh, $updateArray."\n\n");
-    fclose($fh);
-}
+file_get_contents('php://input');
